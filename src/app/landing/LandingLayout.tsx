f055f37b57b0c93de0ca8a8fa2c736f85cf5d855
HoneyHanny <hansduran25@gmail.com>
2024-10-13 23:09:50 +0800

    Fix landing page

--- src/app/landing/LandingLayout.tsx
+++ src/app/landing/LandingLayout.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import LoginPage from '../auth/(auth_layouts)/(login)/Login';
 import RegisterPage from '../auth/(auth_layouts)/(register)/Register';
+import Link from 'next/link';
 // import Image from 'next/image';
 // import OrderLightImage from '../assets/Order_light.png';
 // import HypertextLogo from '../src/assets/HypertextLogo';
@@ -81,29 +82,32 @@ const LandingLayout: React.FC = () => {
     <div className="flex flex-col justify-center h-screen bg-gradient-to-r from-[#5E77D3] to-[#051A41] w-full">
   <nav className="flex justify-between items-center p-6 bg-gradient-to-r from-[#5E77D3] to-[#051A41] shadow-lg w-full">
         <div className="text-white text-2xl font-light">
-         HyperText
+          HyperText
         </div>
         <ul className="flex space-x-10 text-white text-lg">
-          <li className="hover:text-blue-300 cursor-pointer transition duration-300">About</li>
-          <li className="hover:text-blue-300 cursor-pointer transition duration-300">Organizations</li>
+          {/* <li className="hover:text-blue-300 cursor-pointer transition duration-300">About</li>
+          <li className="hover:text-blue-300 cursor-pointer transition duration-300">Organizations</li> */}
         </ul>
         <div>
           
         <div className="flex justify-start items-center space-x-20">
-        <button
-          className="mr-4 text-white hover:text-blue-300 transition duration-300"
-          onClick={goToLogin}
-        >
-          Login
-        </button>
-        <button
-          className="mr-10 bg-gradient-to-r from-[#9AADEC] to-[#5E77D3] text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
-          onClick={goToSignUp}
-        >
-          Sign Up
-        </button>
-</div>
-
+          <Link href={'/auth'}>
+            <button
+              className="mr-4 text-white hover:text-blue-300 transition duration-300"
+              // onClick={goToLogin}
+            >
+              Login
+            </button>
+          </Link>
+          {/* <Link href={'/auth'}>
+            <button
+              className="mr-bg-gradient-to-r from-[#9AADEC] to-[#5E77D3] text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+              onClick={goToSignUp}
+            >
+              Sign Up
+            </button>
+          </Link> */}
+        </div>
         </div>
       </nav>
 
@@ -168,9 +172,11 @@ const LandingLayout: React.FC = () => {
 /> */}
 
   </div>
-  <button className="bg-gradient-to-r from-[#9AADEC] to-[#5E77D3] text-white px-12 py-3 text-lg rounded-md hover:bg-blue-700 mt-8">
-  Get Started
-</button>
+  <Link href='/auth'>
+    <button className="bg-gradient-to-r from-[#9AADEC] to-[#5E77D3] text-white px-12 py-3 text-lg rounded-md hover:bg-blue-700 mt-8">
+      Get Started
+    </button>
+  </Link>
 
 </main>
 
