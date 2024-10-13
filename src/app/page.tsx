212a698c0f57e46f3aece1916147256127fc7731
ivyabapo112102 <abapo@LAPTOP-9709N0SC>
2024-10-13 22:30:53 +0800

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+// import React from 'react';
+
+// export default function Page() {
+
+//   return (
+//     <></>
+//   );
+// };
+
 import React from 'react';
+import LandingLayout from './landing/LandingLayout'; // Import the new LandingLayout
 
 export default function Page() {
-
   return (
-    <></>
+    // <div className="my-auto flex justify-center items-center">
+    <LandingLayout />
+    // </div> 
   );
-};
+}
+
+
