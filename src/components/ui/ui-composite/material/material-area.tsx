6ab7492118597851b45904bda144ff061566c475
kaimatzu <karljohnvillardar@gmail.com>
2024-10-14 12:20:28 +0800

    Workspace functionality (except rearranging module, regenerate content, generate sub-content)

--- src/components/ui/ui-composite/material/material-area.tsx
+++ src/components/ui/ui-composite/material/material-area.tsx
@@ -4,23 +4,24 @@ import ModuleSidenav from './module-sidenav';
 import { useWorkspaceContext } from '@/lib/hooks/context-providers/workspace-context';
 import { Module } from '../module-tree/types';
 import { RiAddFill } from 'react-icons/ri';
+import { insertNode as _insertModuleNode } from '@/app/api/workspace/module/route';
+import RequestBuilder from "@/lib/hooks/builders/request-builder";
 
 const MaterialArea: FC = () => {
-  const { selectedWorkspace, selectedModuleId, selectedModuleData, moduleDataLoading } = useWorkspaceContext();
+  const { selectedWorkspace, selectedModuleId, selectedModuleData, moduleDataLoading, insertModuleNode } = useWorkspaceContext();
   const [treeFormat, setTreeFormat] = useState<Module | null>(null); // Typed state for treeFormat
   
   useEffect(() => {
     if(selectedModuleData && !moduleDataLoading && selectedModuleId) {
-      const module = selectedWorkspace?.modules.find(module => module.id === selectedModuleId);
-      console.log(module, selectedModuleId);
+      const md = selectedWorkspace?.modules.find(module => module.id === selectedModuleId);
+      console.log(md, selectedModuleId);
       console.log("Selected workspace modules:", selectedWorkspace?.modules);
-      if (module) {
-        console.log("Loaded module!", module)
-        setTreeFormat(module);
+      if (md) {
+        console.log("Loaded module!", md)
+        setTreeFormat(md);
       }
-      
     }
-  }, [selectedModuleData, moduleDataLoading, selectedModuleId])
+  }, [selectedModuleData, moduleDataLoading, selectedModuleId, selectedWorkspace?.modules])
 
   return (
     <div className="flex h-full w-full bg-[#F1F3F8]">
@@ -28,7 +29,35 @@ const MaterialArea: FC = () => {
       <div className="max-w-[320px] w-[250px] h-full border border-gray-300 rounded-lg m-2 p-1">
         <div className="flex flex-row justify-between items-center p-1">
             <h1 className="text-sm font-normal transition-none ml-2">Contents</h1>
-            <div className="cursor-pointer mr-2" onClick={() => {}}>
+            <div className="cursor-pointer mr-2" onClick={async (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+
+                const requestBuilder = new RequestBuilder()
+                    .setBody(JSON.stringify({
+                        parentNodeId: selectedModuleId,
+                        moduleId: selectedModuleId,
+                        content: '',
+                        title: 'Untitled',
+                    }));
+
+                const response = await _insertModuleNode(requestBuilder);
+
+                if (response.ok) {
+                    const responseBody = await response.text();
+                    const responseData = JSON.parse(responseBody);
+                    console.log("Data:", responseData);
+
+                    insertModuleNode(selectedWorkspace!.id, selectedModuleId!, {
+                        id: responseData.moduleNodeID as string,
+                        parent: null,
+                        title: 'Untitled',
+                        content: '',
+                        description: 'new node',
+                        children: [],
+                    });
+                }
+            }}>
               <RiAddFill className="w-4 h-4" />
             </div>
         </div>
