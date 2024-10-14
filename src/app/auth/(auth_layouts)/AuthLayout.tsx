48d426334890f610ce6386bf1f0b12fc8670db34
xoreo <edwardjohn.camarillo@cit.edu>
2024-10-14 23:20:00 +0800

    lp styling and auth reroute fix

--- src/app/auth/(auth_layouts)/AuthLayout.tsx
+++ src/app/auth/(auth_layouts)/AuthLayout.tsx
@@ -1,11 +1,20 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginPage from './(login)/Login';
 import RegisterPage from './(register)/Register';
 
 export default function AuthLayout() {
   const [isLoginForm, setIsLoginForm] = useState(true);
+
+  useEffect(() => {
+    // Get the 'mode' query parameter from the URL
+    const searchParams = new URLSearchParams(window.location.search);
+    const mode = searchParams.get('mode');
+
+    // Set the initial state based on the 'mode' value
+    setIsLoginForm(mode === 'login');
+  }, []); // Run only once on mount
 
   const switchForm = () => {
     setIsLoginForm(!isLoginForm);
@@ -13,7 +22,11 @@ export default function AuthLayout() {
 
   return (
     <div>
-      {isLoginForm ? <LoginPage switchForm={switchForm} /> : <RegisterPage switchForm={switchForm} />}
+      {isLoginForm ? (
+        <LoginPage switchForm={switchForm} />
+      ) : (
+        <RegisterPage switchForm={switchForm} />
+      )}
     </div>
   );
 }
