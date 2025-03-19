f8f7f82c35ecc312beced8c74607ff611939f6b3
HoneyHanny <hansduran25@gmail.com>
2025-03-19 21:10:23 +0800

    Remove test connect

--- src/components/ui/ui-composite/transaction/item.tsx
+++ src/components/ui/ui-composite/transaction/item.tsx
@@ -51,7 +51,6 @@ export const StoreItem: React.FC<ItemProps> = ({ item, checkoutWindow, setChecko
         <Button
           className="px-4 py-2 bg-blue-300 text-white rounded hover:bg-blue-100"
           onClick={handleBuyTokens}
-          // onClick={testConnectSocket}
         >
           {item.currency === 'PHP' ? <FaPesoSign /> : <FaDollarSign />} {formattedAmount}
         </Button>
